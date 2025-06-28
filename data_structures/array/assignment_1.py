e5318cad783f5ce6234647d63d8a65ec301261f6
xjmaine <xjfreddie@gmail.com>
2025-06-28 19:31:58 +0000

    Asginement 1: Complete

--- data_structures/array/assignment_1.py
+++ data_structures/array/assignment_1.py
@@ -18,6 +18,9 @@ Question 1.
 - Do not use sorted or array.sort() methods.
 '''
 
+ARRAY_INTEGER_VALIDATION_ERROR = 'Array must be integers only!'
+EMPTY_ARRAY_VALUE_ERROR = 'The array is empty.'
+
 def find_min_max_in_array(*, array: list[int]) -> tuple[int, int]:
     """
     Find the minimum and maximum values in an array.
@@ -26,3 +29,41 @@ def find_min_max_in_array(*, array: list[int]) -> tuple[int, int]:
 
     :return: A tuple containing the minimum and maximum values, with the minimum value first.
     """
+    # Validate input list
+    if not isinstance(array, list):
+        raise ValueError(ARRAY_INTEGER_VALIDATION_ERROR)
+    
+    # Validate array as  int
+    for item in array:
+        if not isinstance(item, int):
+            raise ValueError(ARRAY_INTEGER_VALIDATION_ERROR)
+    
+    # Empty list case
+    if len(array) == 0:
+        print(EMPTY_ARRAY_VALUE_ERROR)
+        return (0, 0)
+    
+    min_val = array[0]
+    max_val = array[0]
+    
+    for value in array[1:]:
+        if value < min_val:
+            min_val = value
+        if value > max_val:
+            max_val = value
+    
+    return (min_val, max_val)
+
+test_array = [7, 5, 1, 3, 5, 0, 9, 10]
+# integer array
+print(find_min_max_in_array(array=test_array))
+
+# single & empty element array
+print(find_min_max_in_array(array=[42]))
+print(find_min_max_in_array(array=[]))
+
+# invalid input criteria
+try:
+    find_min_max_in_array(array=["Ghana", "Accra"])
+except ValueError as e:
+    print(e)
